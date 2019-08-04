36cd58ccd4c4ffad688fed48f17d2a0e1b971cc0
Ludovic Nascimbeni <ludovic.nascimbeni@gmail.com>
2019-08-04 17:31:07 +0200

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 't5djgfjq3ndp',
+        accessToken: '0a-H7s1vtxDSa9b7rfaVR-HrksF1MXE-pqDusLdD_Q8'
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,9 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+import { graphql } from 'gatsby'
+
+export default ({ data: { allContentfulLesson } }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -14,8 +16,29 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    {allContentfulLesson.edges.map(({node}) => (
+      <div>
+        <h1>{node.title}</h1>
+        <img src={node.image.file.url} />
+      </div>
+    ))}
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
-export default IndexPage
+export const query = graphql`
+{
+  allContentfulLesson {
+    edges {
+      node {
+        slug
+        title
+        image {
+          file {
+            url
+          }
+        }
+      }
+    }
+  }
+}`
